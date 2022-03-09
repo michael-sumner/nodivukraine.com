7b24387d9bb50fd944493751102561847cf858be
Michael Bryan Sumner <75835774+michael-sumner@users.noreply.github.com>
2022-03-09 08:57:56 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>No Div Ukraine 🇺🇦</title>
+    <link rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🇺🇦</text></svg>">
+    <meta property="og:title" content="No Div Ukraine | nodivukraine.com">
+    <meta property="og:site_name" content="No Div Ukraine | nodivukraine.com">
+    <meta property="og:url" content="https://nodivukraine.com">
+    <meta property="og:description"
+        content="No Div Ukraine is a website where there is no DIV tag used to create the Ukraine flag. This is to signify to be united as one to stand for humanity.">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://nodivukraine.com/og-image.png">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@nodivukraine">
+    <meta name="twitter:description"
+        content="No Div Ukraine is a website where there is no DIV tag used to create the Ukraine flag. This is to signify to be united as one to stand for humanity.">
+    <meta name="twitter:title" content="No Div Ukraine | nodivukraine.com">
+    <meta name="twitter:image" content="https://nodivukraine.com/og-image.png">
+    <meta name="description"
+        content="No Div Ukraine is a website where there is no DIV tag used to create the Ukraine flag. This is to signify to be united as one to stand for humanity.">
+
+
+    <style>
+        body {
+            margin: 0;
+            --color-ukraine-blue: #0057b7;
+            --color-ukraine-yellow: #ffdd00;
+        }
+
+        body:before {
+            content: "";
+            background: var(--color-ukraine-blue);
+            width: 100%;
+            height: 50%;
+            position: absolute;
+        }
+
+        body:after {
+            content: "";
+            background: var(--color-ukraine-yellow);
+            width: 100%;
+            height: 50%;
+            position: absolute;
+            top: 50%;
+        }
+    </style>
+    <!-- Unity is strength. / issues please send to twitter.com/michaelbsumner -->
+</head>
+
+<body></body>
+
+</html>
